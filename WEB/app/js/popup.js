b0fbc94105b937e740f1b8e019117381765c0d83
vinayramteke <22bee120@nith.ac.in>
2024-03-10 09:18:08 +0530

    modified file with chatbot itegration

--- WEB/app/js/popup.js
+++ WEB/app/js/popup.js
@@ -1,3 +1,27 @@
+// window.onload = function () {
+//   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, { message: "popup_open" });
+//   });
+
+//   document.getElementsByClassName("analyze-button")[0].onclick = function () {
+//     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//       chrome.tabs.sendMessage(tabs[0].id, { message: "analyze_site" });
+//     });
+//   };
+
+//   document.getElementsByClassName("link")[0].onclick = function () {
+//     chrome.tabs.create({
+//       url: document.getElementsByClassName("link")[0].getAttribute("href"),
+//     });
+//   };
+// };
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.message === "update_current_count") {
+//     document.getElementsByClassName("number")[0].textContent = request.count;
+//   }
+// });
+
 window.onload = function () {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { message: "popup_open" });
@@ -9,10 +33,11 @@ window.onload = function () {
     });
   };
 
-  document.getElementsByClassName("link")[0].onclick = function () {
-    chrome.tabs.create({
-      url: document.getElementsByClassName("link")[0].getAttribute("href"),
-    });
+  // Add event listener to the "Chat bot" button
+  document.querySelector('.fb a').onclick = function (event) {
+    event.preventDefault(); // Prevent default action of the anchor tag
+    var chatUrl = this.getAttribute('href'); // Get the href attribute
+    chrome.tabs.create({ url: chatUrl }); // Open chat.html in a new tab
   };
 };
 
@@ -21,3 +46,4 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     document.getElementsByClassName("number")[0].textContent = request.count;
   }
 });
+
